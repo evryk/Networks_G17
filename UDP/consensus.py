cfd30892dcb35e05eafb62b888f578fd0c1031b4
evryk <127775299+evryk@users.noreply.github.com>
2024-03-23 23:08:53 +0000

    updated listener to support new packet format
    not finished + worked some more on consensus packet functions (decoding & encoding)

--- UDP/consensus.py
+++ UDP/consensus.py
@@ -4,6 +4,7 @@ from typing import List
 import uuid
 import struct
 
+MAGIC = 0x01051117
 
 class PcktID(Enum):
     hello_c2s = 0  # from client to server, basically SYN
@@ -14,7 +15,7 @@ class PcktID(Enum):
     vote_s2c_broadcast_result = 5  # from server to all clients
 
 @dataclass
-class PcktHeader:
+class DataHeader:
     Magic: int  # 4 bytes
     Checksum: int  # 4 bytes CRC32
     PcktID: PcktID  # 2 bytes
@@ -27,7 +28,7 @@ class HelloPacket(Enum):
 
 @dataclass
 class PcktHello:
-    Header: PcktHeader  # 8 bytes
+    Header: DataHeader  # 8 bytes
     Version: int  # 4 bytes
     NumFeatures: int  # 2 bytes
     Feature: List[int]  # num_features*2 bytes
@@ -44,8 +45,8 @@ class Response(Enum):
 
 @dataclass
 class PcktVoteRequest:
-    Header: PcktHeader  # 8 bytes
-    VoteID: uuid.UUID  # 16 bytes
+    Header: DataHeader  # 8 bytes
+    VoteID: uuid.UUID  # 16 bytes # uuid.uuid4() # str(uuid.uuid4()) # uuid.uuid4().hex
     QuestionLength: int  # 4 bytes
     Question: str  # QuestionLength bytes long
 
@@ -53,8 +54,45 @@ PcktVoteBroadcast = PcktVoteRequest
 
 @dataclass
 class PcktVoteResponse:
-    Header: PcktHeader  # 8 bytes
+    Header: DataHeader  # 8 bytes
     VoteID: uuid.UUID  # 16 bytes
     Response: Response  # 2 bytes
 
 PcktVoteResultBroadcast = PcktVoteResponse
+
+#How can I iterate through a byte array, and extract certain bytes to decode and assign to each parameter in my classes? How do I encode these parameters back into bytes?
+
+def encode_dataHeader(header):
+    return struct.pack('!IIH', header.Magic, header.Checksum, header.PcktID.value)
+
+def decode_dataHeader(data):
+    magic, checksum, pckt_id = struct.unpack('!IIH', data[:10])
+    return DataHeader(Magic=magic, Checksum=checksum, PcktID=PcktID(pckt_id))
+
+
+# PcktHello
+def encode_Hello(packet):
+    header = encode_dataHeader(packet.Header)
+    features = b''.join(struct.pack('!H', feature) for feature in packet.Feature)
+    return struct.pack('!10sIH' + 'H'*len(features), header, packet.Version, packet.NumFeatures, *features)
+
+def decode_Hello(data):
+    header = decode_dataHeader(data[:10])
+    version, num_features = struct.unpack('!IH', data[10:16])
+    features = [struct.unpack('!H', data[i:i+2])[0] for i in range(16, 16 + 2*num_features, 2)]
+    return PcktHello(Header=header, Version=version, NumFeatures=num_features, Feature=features)
+
+
+# PcktHelloResponse
+def encode_HelloResponse(packet):
+    header = encode_dataHeader(packet.Header)
+    features = b''.join(struct.pack('!H', feature) for feature in packet.Feature)
+    return struct.pack('!10sIH' + 'H'*len(features), header, packet.Version, packet.NumFeatures, *features)
+
+def decode_HelloResponse(data):
+    header = decode_dataHeader(data[:10])
+    version, num_features = struct.unpack('!IH', data[10:16])
+    features = [struct.unpack('!H', data[i:i+2])[0] for i in range(16, 16 + 2*num_features, 2)]
+    return PcktHelloResponse(Header=header, Version=version, NumFeatures=num_features, Feature=features)
+
+# 
--- UDP/listener.py
+++ UDP/listener.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 from conversation import conversation
+import packet
 
 # This is a Dictionary of all the current objects,
 # where each Object's Key in the dictionary is the conversation's ID
@@ -10,20 +11,15 @@ def listener(server_socket):
     while True:
         data, client_address = server_socket.recvfrom(4096)
         if data:
-            string_data = data.decode()
-            
-            # extract client ID
-            client_id = string_data[0:4]
+            # Extract Packet Header
+            pckt_header = packet.decode_header(data[:20])
 
             # check client ID against list of ongoing conversations (here this will call a checker function,
             # that returns a pointer or reference to a pre-existing or new conversation object)
-            conversation_object = conversation_checker(client_id)
+            conversation_object = conversation_checker(pckt_header.ConvID)
 
             # send message to corresponding conversation (using corresponding reference returned from checker function)
-            conversation_object.receive_packet(string_data[4:])
-
-            #print(f"{client_address}, id {client_id}")
-            server_socket.sendto(b"ACKed", client_address) # here for now, will be in tcp functions later
+            conversation_object.receive_packet(data[20:])
 
 
 
