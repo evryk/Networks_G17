957205d13dc217bbc5b0a33ef4ab8d0c4569be2b
maxmusumeci <maxxmusumeci@gmail.com>
2024-03-23 15:01:46 -0700

    started selective repeat

--- UDP/conversation.py
+++ UDP/conversation.py
@@ -48,11 +48,50 @@ class conversation:
     def handshake():
         pass
 
-    def selective_repeat():
+    class selective_repeat():
+      def __init__(self, window_size, timeout):
+        self.window_size = window_size;
+        self.buffer = [None] * window_size # please initialize buffer here
+        self.expected_seq_num = 0
+
+
+      def send_packet(self, packet):
         pass
+        # implement sending logic here
+
+      def receive_packet(self, packet):
+        seq_num = packet.Header.PcktID
+        if seq_num == self.expected_seq_num:
+          # Packet received in order
+          self.buffer[seq_num % self.window_size] = packet
+          self.expected_seq_num += 1
+          self.send_ack(seq_num)
+        else:
+          pass
+          # buffer here
+
+
+      def send_ack(self, seq_num):
+        ack_packet = PcktHeader(0, 0, seq_num)
+        ack_packet_type = packet_types.get(seq_num)
+        if ack_packet_type:
+          ack_packet = ack_packet_type(ack_packet)
+          self.send_packet(ack_packet)
+
+     
+#usage (sample, put this in driver file)
+
+packet_types = {
+    PcktID.hello_c2s.value: PcktHello,
+    PcktID.hello_back_s2c.value = PcktHelloResponse,
+    # add more types
+
+}
+        
 
     def tcp_cubic():
         pass
+# Splitting up packet without fragmentation
 
     def fragmentator():
         pass
