cb319f8723ee300c752226f19a9fea37572872fd
evryk <127775299+evryk@users.noreply.github.com>
2024-03-26 15:51:43 +0000

    initial functions defined

---
+++ UDP/actions.py
@@ -0,0 +1,28 @@
+# Based on PcktID --> consensus.py file
+import consensus
+
+# Defing SYN function for Client to server.
+def SYN_send():
+    pass
+
+# Defining SYN_ACK function for Server to Client
+def SYN_ACK():
+    pass
+
+# Defining Vote Request Packet - initial question client to server
+def VoteRequest():
+    pass
+
+# Defining Vote Broadcast Packet - server to all client nodes
+def VoteBroadcast():
+    pass
+
+# Defining Vote Response Packet - all client nodes to server
+def VoteResponse():
+    pass
+
+# Defining Vote Result Broadcast Packet - server to all client nodes
+def ResultBroadcast():
+    pass
+
+
